dd8b752db9651d7415bbccdaafe859e0494d1403
dvankevich <dvankevich@gmail.com>
2026-02-17 23:13:53 +0200

    chore: TMDB API test

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,11 @@
 declare module "astro-htmx";
 declare module "@astrojs/preact";
 declare module "@astrojs/netlify";
+
+interface ImportMetaEnv {
+  readonly TMDB_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
